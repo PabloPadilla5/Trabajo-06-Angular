61c80ff744f133d83f6bb8d7655d13a2d5251bd3
Pablo Padilla <pablopadillamaldonado@gmail.com>
2022-03-14 13:12:39 -0600

    nfhbhfidnbfidjnif

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/hola-angular'));
+app.use(express.static(__dirname + '/dist/hola_angular'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname + '/dist/hola-angular/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/hola_angular/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
